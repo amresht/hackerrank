311a9ae76a9818895e2f1355710bd8e3525a9d8c
amresh tripathi <amresh2k@gmail.com>
2020-10-06 13:08:30 +0530

    array reverse in python

---
+++ ds/arrayreverse.py
@@ -0,0 +1,60 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the reverseArray function below.
+def reverseArray(a):
+    arr2 = list()
+    length = len(a)
+    for i in range(length):
+        print(i)
+        arr2.append( a[length-i-1])
+    return arr2
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the reverseArray function below.
+def reverseArray(a):
+    arr2 = list()
+    length = len(a)
+    for i in range(length):
+        print(i)
+        arr2.append( a[length-i-1])
+    return arr2
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
