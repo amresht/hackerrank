a9ce0401a2d5605f0a3d5fa13c50c1f1e7498af3
amresh tripathi <amresh2k@gmail.com>
2020-10-10 10:52:40 +0530

    python linked list

---
+++ ds/linkedlist.py
@@ -0,0 +1,51 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# lINKED list in python\n",
+    "# @AUTHOR - AMRESH TRIPATHI\n",
+    "#\n",
+    "\n",
+    "class Node:\n",
+    "    \n",
+    "    def __init__(self, name):\n",
+    "        self.name = name\n",
+    "        self.prev  = None    # OPTIONAL for the double linked lists\n",
+    "        self.next =  None    # link to next node\n",
+    "\n",
+    "class Linkedlist:\n",
+    "    # initialize with no values \n",
+    "    def __init__(self, head=None):\n",
+    "        self.head = head\n",
+    "        \n",
+    "        \n",
+    "        "
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
