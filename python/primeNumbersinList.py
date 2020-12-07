c39bdf2e0e3223ea56d048b10648ebbddc9cec7b
amresh tripathi <amresh2k@gmail.com>
2020-12-07 15:16:00 +0530

    Print all prime numbers in a list

--- python/primeNumbersinList.py
+++ python/primeNumbersinList.py
@@ -8,6 +8,7 @@ def is_prime(x):
     sqrtx = int(math.sqrt(x))
     
     for  i in range(2,sqrtx + 1):
+        #Since we found a multiple other than 1,itself it is not prime
         if x % i == 0 :
             return False
     return True
@@ -15,10 +16,14 @@ def is_prime(x):
 #Code execution starts here 
 if __name__=='__main__': 
     
+#Let us define a list for simplicity
 number_list = [2,51, 5, 10, 18,13, 20, 14, 5 , 7 ,9] 
-    
+
+#For each number in the list    
 for i in range(len(number_list)):
+    #Check if it is a prime number
     if(is_prime(number_list[i])):
+        #it is a prime so we print it
        print(number_list[i]) 
 
 
