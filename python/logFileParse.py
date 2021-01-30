cda37aa6aa2301d97d7f941b741ae2bea55ed0b4
amresh tripathi <amresh2k@gmail.com>
2021-01-30 14:20:48 +0530

    changes to logfile parse , pseudo code

--- python/logFileParse.py
+++ python/logFileParse.py
@@ -18,27 +18,56 @@ output_filename = os.path.normpath("output/parsed_lines.log")
 
 #global variable for error count 
 error_count =  0;
+#global variable for error text 
+error_text =  [];
 
+#Run infinitely
+while True:
 
-# Overwrites the file, ensure we're starting out with a blank file
-with open(output_filename, "w") as out_file:
-    out_file.write("")
-
-# Open output file in 'append' mode
-with open(output_filename, "a") as out_file:
-    # Open input file in 'read' mode
     with open(input_filename, "r") as in_file:
         # Loop over each log line
         for line in in_file:
             # If log line matches our regex, print to console, and output file
             if (line_regex.search(line)):
                 error_count = error_count+1
-                print line
+                List.append(line) 
                 out_file.write(line)
 
 # this method will rename the file name to a backup path
 def rename_file(old_filename, new_filename):
     os.rename(r'old_filename',r'old_filename')
 
-       
+def send_email(body):
+    pass
+"""
+//pseudo code
     
+    
+while true
+    check file size
+    if file_size > 10 GB 
+        rename current file as bkp 
+        create new blank file
+        
+    if SIGNTERM 
+        rename current file as bkp 
+        exit script
+    
+    
+    check file size
+    if file_size > 10 GB 
+        rename current file as bkp 
+        create new blank file
+
+    
+    read file line by line
+    
+    // if the log file matches and time is within 8:00 18:00
+    write to output log
+        error_list.append (error_instance)
+
+    
+    if time%% 60s 
+        send email 
+        empty  error_list
+"""
