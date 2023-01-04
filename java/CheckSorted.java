89c23e646efdfd9a76c9d6f4d4325ad7c1fe792f
Amresh Tripathi <a.tripathi@us.411locals.com>
2023-01-04 15:07:09 +0530

    arrays problems

---
+++ java/CheckSorted.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class CheckSorted {
+
+static boolean isSorted(int arr[]) {
+    int n = arr.length;
+    for (int i =0; i<n ; i++) {
+        if(arr[i+1] < arr[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+    public static void main(String args[]) throws IOException {
+        int arr[] = {1, 2, 11, 34, 98, 113, 10, 1123};
+        System.out.println(isSorted(arr));
+    }   
+}
---
+++ java/LargestInteger.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class LargestInteger {
+
+    static int getLargest(int arr[]) {
+        int n = arr.length;
+        for (int i=0; i < n ;i++){
+            boolean flag = true;
+            for (int j=i; j<n; j++) {
+                if( arr[j] > arr[i]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag==true)
+                return i;
+            }
+        return -1;
+    }
+    
+
+    public static void main(String args[]) throws IOException {
+        int arr[] = {3, 2, 11, 34, 98, 13, 10, 123};
+        System.out.print("Index of Largest number is=> ");
+        System.out.println(getLargest(arr));
+
+    }
+}
