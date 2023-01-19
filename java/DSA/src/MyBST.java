9d9114f8d09a73a6e503c6529d44d2d940917363
Amresh Tripathi <a.tripathi@us.411locals.com>
2023-01-19 18:13:21 +0530

    Created Binary Search Tree insert

--- java/DSA/src/MyBST.java
+++ java/DSA/src/MyBST.java
@@ -15,7 +15,47 @@ public class MyBST {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Node root = new Node(10);
+		
+		root.left = new Node(5);
+		root.right = new Node(15);
+		root.right.left = new Node(12);
+		root.right.right = new Node(18);
+		
+    	int x=20;
+    	
+    	root=insert(root,x);
+    	inOrderTraversal(root);		
 
 	}
 
+	
+	
+	public static Node insert(Node root, int x) {
+		// if the tree is null
+		if (root == null) {
+			// create a new node and return as root
+			return new Node(x);
+		}
+		
+		// if the key being inserted is greater than root node's key insert on right subtree 
+		if(root.key < x) {
+			root.right = insert(root.right, x);
+		}
+		else if (root.key >= x) {
+			//if the key being inserted is lesser than root node's key insert on left subtree			
+			root.left = insert(root.left, x);
+		}
+		return root;
+	}
+	
+	public static void inOrderTraversal(Node root) {
+		// if root is not null 
+		if (root != null) {
+			inOrderTraversal(root.left);
+			System.out.print(root.key + " " );
+			inOrderTraversal(root.right);
+		}
+		return;
+	}	
+	
 }
